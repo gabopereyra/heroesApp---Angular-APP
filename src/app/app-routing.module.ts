e19ac30a4dad24f8d35748f1d20d8f4eaebcdefc
gabopereyra <gabrielp@unitech.com.ar>
2023-01-19 07:31:16 -0300

    Add readme.md
    Add dependencies: Angular Material
    
    Add structure and initial files, with the configuration for lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './shared/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path: '404',
+    component: ErrorComponent
+  },
+  {
+    path: '**',
+    component: ErrorComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
