8cfce4408a2982b1b428851727814e4e54ad996d
gabopereyra <gabrielp1689@gmail.com>
2023-01-23 21:36:45 -0300

    Add enviroment variable and update service in accordance with the mentioned change

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,21 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Heroe } from '../interfaces/heroe.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private url: string = environment.urlBase;
+
   constructor(
     private http: HttpClient
   ) {  }
 
   getHeroes() : Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.url}/heroes`);
   }
 
   getHeroeById(id: string) : Observable<Heroe> {
-    return this.http.get<Heroe>('http://localhost:3000/heroes/'+id);
+    return this.http.get<Heroe>(`${this.url}/heroes/${id}`);
   }
 }
